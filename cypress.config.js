6097b6758048a3dab704c400f70e0ba275a3e059
Shawn Harris <shawn.harris3510@gmail.com>
2023-02-27 10:53:43 -0800

    fixing config

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,26 @@
 const { defineConfig } = require("cypress");
-const _ = require('lodash')
-const del = require('del')
+// need to install these dependencies
+// npm i lodash del --save-dev
+const _ = require("lodash");
+const del = require("del");
 
 module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
+      on("after:spec", (spec, results) => {
         if (results && results.video) {
           // Do we have failures for any retry attempts?
           const failures = _.some(results.tests, (test) => {
-            return _.some(test.attempts, { state: 'failed' })
-          })
+            return _.some(test.attempts, { state: "failed" });
+          });
           if (!failures) {
             // delete the video if the spec passed and no tests retried
-            return del(results.video)
+            return del(results.video);
           }
         }
-      })
+      });
     },
   },
 });

3997a62222c5cb91ca15dc148ec6ace353318ab2
Shawn Harris <shawn.harris3510@gmail.com>
2023-02-27 10:46:25 -0800

    test delete video on condition

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = _.some(results.tests, (test) => {
+            return _.some(test.attempts, { state: 'failed' })
+          })
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            return del(results.video)
+          }
+        }
+      })
     },
   },
   videoUploadOnPasses: false

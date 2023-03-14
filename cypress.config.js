680d06cb8799954ae463ba6dfc389d600ecaec73
Shawn Harris <shawn.harris3510@gmail.com>
2023-03-14 16:08:34 -0700

    config2.yml w/ conditional

--- cypress.config.js
+++ cypress.config.js
@@ -9,18 +9,18 @@ module.exports = defineConfig({
   // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      // on('after:spec', (spec, results) => {
-      //   if (results && results.video) {
-      //     // Do we have failures for any retry attempts?
-      //     const failures = _.some(results.tests, (test) => {
-      //       return _.some(test.attempts, { state: 'failed' })
-      //     })
-      //     if (!failures) {
-      //       // delete the video if the spec passed and no tests retried
-      //       return del(results.video)
-      //     }
-      //   }
-      // })
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = _.some(results.tests, (test) => {
+            return _.some(test.attempts, { state: 'failed' })
+          })
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            return del(results.video)
+          }
+        }
+      })
     },
   },
 })

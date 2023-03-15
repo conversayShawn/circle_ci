22f5373d6ce52048133f30ef05e4e2c4401700f5
Shawn Harris <shawn.harris3510@gmail.com>
2023-03-15 12:36:27 -0700

    config2.yml w/ videoUploadOnPasses

--- cypress.config.js
+++ cypress.config.js
@@ -5,22 +5,21 @@ const _ = require('lodash')
 const del = require('del')
 
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
+  videoUploadOnPasses: false,
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = _.some(results.tests, (test) => {
-            return _.some(test.attempts, { state: 'failed' })
-          })
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            return del(results.video)
-          }
-        }
-      })
+      // on('after:spec', (spec, results) => {
+      //   if (results && results.video) {
+      //     // Do we have failures for any retry attempts?
+      //     const failures = _.some(results.tests, (test) => {
+      //       return _.some(test.attempts, { state: 'failed' })
+      //     })
+      //     if (!failures) {
+      //       // delete the video if the spec passed and no tests retried
+      //       return del(results.video)
+      //     }
+      //   }
+      // })
     },
   },
 })

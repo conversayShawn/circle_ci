918a3af48d136f2898cd2bccf23d0c019c6f1f18
Shawn Harris <shawn.harris3510@gmail.com>
2023-04-04 18:44:42 -0700

    _ to results.tests & test.attempts

--- cypress.config.js
+++ cypress.config.js
@@ -10,8 +10,8 @@ module.exports = defineConfig({
       on('after:spec', (spec, results) => {
         if (results && results.video) {
           // Do we have failures for any retry attempts?
-          const failures = spec.some(results.tests, (test) => {
-            return spec.some(test.attempts, { state: 'failed' })
+          const failures = results.tests.some( (test) => {
+            return test.attempts.some( { state: 'failed' })
           })
           if (!failures) {
             // delete the video if the spec passed and no tests retried
